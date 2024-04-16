abab99f96a227cc0fe723f1ffc7e27111ff4e232
katalinjordans <katalinjordans@gmail.com>
2024-04-16 20:05:11 +0200

    Refactor: updated name of the main class from Application to RecruitmentApp, to avoid confusion

--- src/main/java/com/ironhack/RecruitmentApp.java
+++ src/main/java/com/ironhack/RecruitmentApp.java
@@ -3,7 +3,7 @@ package com.ironhack;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class RecruitmentApp {
 
     public static void main(String[] args) {
 
