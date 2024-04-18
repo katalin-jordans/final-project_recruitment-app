fb76e8e9dbd7c4724dc5efdf8e663664309bb721
katalinjordans <katalinjordans@gmail.com>
2024-04-18 20:24:56 +0200

    Feat: create Application model, controller, repository and service classes

---
+++ src/main/java/com/ironhack/controller/ApplicationController.java
@@ -0,0 +1,30 @@
+package com.ironhack.controller;
+
+import com.ironhack.model.Application;
+import com.ironhack.model.Job;
+import com.ironhack.repository.ApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/applications")
+public class ApplicationController {
+
+    @Autowired
+    private ApplicationRepository applicationRepository;
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<Application> getAll() {
+        return applicationRepository.findAll();
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<Application> addJob() {
+        return applicationRepository.findAll();
+    }
+}
---
+++ src/main/java/com/ironhack/service/ApplicationService.java
@@ -0,0 +1,11 @@
+package com.ironhack.service;
+
+import com.ironhack.repository.ApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationService {
+    @Autowired
+    private ApplicationRepository applicationRepository;
+}
