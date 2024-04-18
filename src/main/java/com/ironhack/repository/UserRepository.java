ac4d958c0536d38eaa549fbdb070cbf68a6a6756
katalinjordans <katalinjordans@gmail.com>
2024-04-18 19:01:38 +0200

    Feat: user model

---
+++ src/main/java/com/ironhack/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ironhack.repository;
+
+import com.ironhack.model.Recruiter;
+import com.ironhack.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
