1fa9bb4c3d4e78ec7c09d35a42e94d9aa42b6fc9
katalinjordans <katalinjordans@gmail.com>
2024-04-18 19:08:07 +0200

    Feat: adding HiringManger class

---
+++ src/main/java/com/ironhack/model/HiringManager.java
@@ -0,0 +1,15 @@
+package com.ironhack.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+public class HiringManager extends User {
+}
